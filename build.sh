6f46b8e1d54f7b61267096d0d2f2cd61a52c2989
David Craddock <contact@davidcraddock.net>
2026-06-07 20:18:51 +0100

    Build command I used

---
+++ build.sh
@@ -0,0 +1,3 @@
+arduino-cli compile \                                                                                                              took 17s │ ✔
+  --fqbn megaTinyCore:megaavr:atxy6:chip=1616,clock=20internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=8,wiremode=mors,printf=default,PWMmux=A_default,attach=allenabled,WDTtimeout=disabled,WDTwindow=disabled --build-path ./compiled .
+
