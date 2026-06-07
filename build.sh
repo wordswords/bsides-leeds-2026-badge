a6cd4fff55fcf96a5b71e757a71b355b797f740e
David Craddock <contact@davidcraddock.net>
2026-06-07 20:20:06 +0100

    The build steps I used

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+arduino-cli config init
+arduino-cli config set board_manager.additional_urls
+https://drazzy.com/package_drazzy.com_index.json
+arduino-cli core update-index
+arduino-cli core install megaTinyCore:megaavr
+
 arduino-cli compile \                                                                                                              took 17s │ ✔
   --fqbn megaTinyCore:megaavr:atxy6:chip=1616,clock=20internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=8,wiremode=mors,printf=default,PWMmux=A_default,attach=allenabled,WDTtimeout=disabled,WDTwindow=disabled --build-path ./compiled .
 
